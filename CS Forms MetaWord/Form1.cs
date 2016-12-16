8130e6d20de1d4ea565ae5ae41c8d1452407d854
Mark Filter <markfilter@outlook.com>
2016-12-16 00:21:10 -0500

    Adds refactored code with comments.

--- CS Forms MetaWord/Form1.cs
+++ CS Forms MetaWord/Form1.cs
@@ -12,11 +12,11 @@ namespace CS_Forms_MetaWord
 {
     public partial class Form1 : Form
     {
-
+        // Private Member Variable
         private string textEditorContents;
 
 
-
+        // Form Constructor
         public Form1()
         {
             InitializeComponent();
@@ -24,32 +24,47 @@ namespace CS_Forms_MetaWord
             statusLabelCharCount.Text = "Character Count: 0";
         }
 
+        // File > Quilt
         private void quitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
+        // Tool Strip > Clear
         private void tsBtnClear_Click(object sender, EventArgs e)
         {
             rtbTextEditor.Text = "";
-            statusLabelWordCount.Text = "Word Count: 0";
-            statusLabelCharCount.Text = "Character Count: 0";
         }
 
+        // Clear Button (bottom of the form)
         private void btnClear_Click(object sender, EventArgs e)
         {
             rtbTextEditor.Text = "";
-            statusLabelWordCount.Text = "Word Count: 0";
-            statusLabelCharCount.Text = "Character Count: 0";
         }
 
+        // Event: Text Changed
+        // updates the word and character count by converting the text
+        // into a string array for word count and a character array
+        // for a character count.
         private void rtbTextEditor_TextChanged(object sender, EventArgs e)
         {
-            textEditorContents = rtbTextEditor.Text;
-            string[] words = textEditorContents.Split(' ');
-            statusLabelWordCount.Text = $"Word Count: {words.Length}";
-            char[] characters = textEditorContents.ToCharArray();
-            statusLabelCharCount.Text = $"Character Count: {characters.Length}";
+            if(rtbTextEditor.Text != "")
+            {
+                textEditorContents = rtbTextEditor.Text;
+                string[] words = textEditorContents.Split(' ');
+                statusLabelWordCount.Text = $"Word Count: {words.Length}";
+                char[] characters = textEditorContents.ToCharArray();
+                statusLabelCharCount.Text = $"Character Count: {characters.Length}";
+            }
+            else
+            {
+                textEditorContents = rtbTextEditor.Text;
+                string[] words = textEditorContents.Split(' ');
+                statusLabelWordCount.Text = "Word Count: 0";
+                char[] characters = textEditorContents.ToCharArray();
+                statusLabelCharCount.Text = "Character Count: 0";
+            }
+            
         }
     }
 }
